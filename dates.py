14969517d415b2a6121e5c38a884bbc906f0e19f
ilyasoffahtemka <akhtem.ilyasov@gmail.com>
2024-08-16 03:40:41 +0300

    Add files via upload

---
+++ dates.py
@@ -0,0 +1,18 @@
+print("hello word") # //-еллочисленное деление /- деление
+print(type("hello word")) #string - строка
+print('hello ' +'word')   #concatenete
+print('1' + '1')
+print(1+1)
+print(True, False) #boolean
+print(type(True), type(False))
+print(5>10, 10>5)
+print(1,2,3,4,'hello,word', True)
+print(5<=5)
+print(5>=5)
+print(5==5)
+print(5!=5)
+print(5!=5 and 5<10)
+print(5==5 and 5<10)
+print(5!=5 or 5<10)
+print(int('5'))
+print(type(int('5')))
